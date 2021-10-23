10f230c017885215c81d38218346a56a7b3c159d
Liran Tal <liran.tal@gmail.com>
2021-10-23 12:42:05 +0300

    feat: support customized content around data

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,33 @@
 #!/usr/bin/env node
+import { readFile } from 'fs/promises'
+import path from 'path'
 import { generateGigs } from '../src/main.js'
+import json2md from 'json2md'
 
-const result = await generateGigs()
-console.log(result)
+const __dirname = process.cwd()
+const jsonConfigFileContents = await readFile(
+  path.join(__dirname, 'gigsboat.json'),
+  'utf8'
+)
+const gigsConfig = JSON.parse(jsonConfigFileContents)
+
+const gigsMarkdown = await generateGigs()
+
+let markdownOutput = ''
+for (const [hook, contents] of Object.entries(gigsConfig)) {
+  if (hook === 'preContent') {
+    for (const content of contents) {
+      if (content.hasOwnProperty('raw')) {
+        markdownOutput += content.raw + '\n'
+      }
+
+      if (content.hasOwnProperty('format')) {
+        markdownOutput += json2md(content.format) + '\n'
+      }
+    }
+  }
+
+  markdownOutput += gigsMarkdown + '\n'
+}
+
+console.log(markdownOutput)
