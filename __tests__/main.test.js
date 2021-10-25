d8bff1e9146f764089e32f3f9a321b7300d7d839
Liran Tal <liran.tal@gmail.com>
2021-10-25 23:32:03 +0300

    test: more and more coverage woohoo

---
+++ __tests__/main.test.js
@@ -0,0 +1,42 @@
+import { URL } from 'url'
+import path from 'path'
+import { generateDocument } from '../src/main.js'
+
+const __dirname = new URL('.', import.meta.url).pathname
+
+test('given a directory of markdown files a full document is rendered', async () => {
+  const filePath = path.join(__dirname, './__fixtures__/main-datafiles')
+  const jsonResult = await generateDocument(filePath)
+  expect(jsonResult).toMatchSnapshot()
+})
+
+test('given a directory of markdown files a full document is rendered along with pre and post content', async () => {
+  const filePath = path.join(__dirname, './__fixtures__/main-datafiles')
+  const jsonResult = await generateDocument({
+    preContent: [
+      {
+        raw: '<p> actual html is allowed too </p>'
+      },
+      {
+        format: [
+          {
+            p: 'Liran Tal'
+          }
+        ]
+      }
+    ],
+    postContent: [
+      {
+        raw: '<p align="center"> rendered in postcontent </p>'
+      },
+      {
+        format: [
+          {
+            p: 'Thank you!'
+          }
+        ]
+      }
+    ]
+  })
+  expect(jsonResult).toMatchSnapshot()
+})
