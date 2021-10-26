12884a90697d646a7240250fe6541daea17aa1ee
Liran Tal <liran.tal@gmail.com>
2021-10-27 00:02:21 +0300

    test: fix tests

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -6,13 +6,14 @@ const __dirname = new URL('.', import.meta.url).pathname
 
 test('given a directory of markdown files a full document is rendered', async () => {
   const filePath = path.join(__dirname, './__fixtures__/main-datafiles')
-  const jsonResult = await generateDocument(filePath)
+  const jsonResult = await generateDocument({ sourceDirectory: filePath })
   expect(jsonResult).toMatchSnapshot()
 })
 
 test('given a directory of markdown files a full document is rendered along with pre and post content', async () => {
   const filePath = path.join(__dirname, './__fixtures__/main-datafiles')
   const jsonResult = await generateDocument({
+    sourceDirectory: filePath,
     preContent: [
       {
         raw: '<p> actual html is allowed too </p>'
