890b4acfbe881f478ea7fe49f25ff41aeeb9a6c4
Liran Tal <liran.tal@gmail.com>
2021-10-30 21:42:31 +0300

    feat: support config schema validation and zero config for defaults

--- bin/config-manager.js
+++ bin/config-manager.js
@@ -1,20 +1,60 @@
 import { promises as fs } from 'fs'
+import ajv from 'ajv'
 import path from 'path'
 
 const configFileName = 'gigsboat.json'
 const __dirname = process.cwd()
 
 export async function getConfig() {
-  const jsonConfigFileContents = await fs.readFile(
-    path.join(__dirname, configFileName),
-    'utf8'
-  )
-  const gigsConfig = JSON.parse(jsonConfigFileContents)
+  let gigsConfig = {}
+  try {
+    const jsonConfigFileContents = await fs.readFile(
+      path.join(__dirname, configFileName),
+      'utf8'
+    )
+    gigsConfig = JSON.parse(jsonConfigFileContents)
+  } catch (error) {
+    // debug
+    // console.error(error)
+  }
 
-  // @TODO
-  // add AJV validation here for the schema
-  // in another function and use defaults
-  // like here: https://ajv.js.org/guide/modifying-data.html#assigning-defaults
-
+  validateConfig(gigsConfig)
   return gigsConfig
+}
+
+function validateConfig(config) {
+  const gigsConfigSchema = {
+    type: 'object',
+    properties: {
+      input: {
+        type: 'object',
+        properties: {
+          sourceDirectory: { type: 'string', default: 'pages' }
+        },
+        default: {}
+      },
+      output: {
+        type: 'object',
+        properties: {
+          markdownFile: { type: 'string', default: 'README-gigsfile.md' }
+        },
+        default: {}
+      },
+      preContent: {
+        type: 'array',
+        items: {
+          type: 'object',
+          properties: {
+            raw: { type: 'string' },
+            format: { type: 'array', items: { type: 'object' } }
+          }
+        }
+      }
+    }
+  }
+
+  const schemaValidator = new ajv({ useDefaults: true })
+  const schemaValidation = schemaValidator.compile(gigsConfigSchema)
+  schemaValidation(config)
+  return config
 }
