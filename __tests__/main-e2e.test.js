69358544b3f17b3764e89992eda7b84206927417
Liran Tal <liran.tal@gmail.com>
2022-08-06 15:23:38 +0300

    test: extend e2e tests to include matching the markdown file
    Add a specific TEST_E2E flag to the generated file to indicate
    that timestamps shouldn't be generated in the output file so
    that we can always use the .toMatchSnapshot() assertion for
    the entire contents of the file.

--- __tests__/main-e2e.test.js
+++ __tests__/main-e2e.test.js
@@ -1,6 +1,6 @@
 import { spawnSync } from 'child_process';
 import { tmpdir } from 'os';
-import { unlinkSync } from 'fs'
+import { unlinkSync, readFileSync } from 'fs'
 import path from 'path';
 
 const __dirname = new URL('.', import.meta.url).pathname
@@ -10,13 +10,23 @@ test('spawn the cli to process data and ensure markdown file is generated correc
     const projectFixtures = path.join(__dirname, './__fixtures__/main-datafiles/myapp/pages')
     const projectOutputFile = path.join(tmpdir(), 'test.md')
 
-    const cliData = spawnSync(cliExecutable, ['--source-directory', projectFixtures, '--output-file', projectOutputFile], { shell: true});
+    
+    // specifically need to mutate the process.argv array to avoid the cli
+    // generating an "updated at" timestamp so we can always match the snapshot
+    // and so we add a TEST_E2E flag to it
+    const spawnedCliEnvironmentVariables = process.env
+    spawnedCliEnvironmentVariables.TEST_E2E = 'true'
+
+    const cliData = spawnSync(cliExecutable, ['--source-directory', projectFixtures, '--output-file', projectOutputFile], { shell: true, env: spawnedCliEnvironmentVariables})
 
     const cliOutput = cliData.output.toString();
     
     expect(cliOutput).toContain('loaded configuration:')
     expect(cliOutput).toContain('source directory: ' + projectFixtures)
     expect(cliOutput).toContain('output file: ' + projectOutputFile)
+
+    const expectedMarkdownOutput = readFileSync(projectOutputFile, 'utf8')
+    expect(expectedMarkdownOutput).toMatchSnapshot()
 
     // cleanup temporary files
     unlinkSync(projectOutputFile)
