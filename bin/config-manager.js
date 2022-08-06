5f5b3e8d32ac7c5cad8e49f39485a1ecaba60cf7
Liran Tal <liran.tal@gmail.com>
2022-08-06 13:33:04 +0300

    feat: feat/add debug capabilities (#39)
    * feat: add debugger support

--- bin/config-manager.js
+++ bin/config-manager.js
@@ -1,13 +1,17 @@
 import { promises as fs } from 'fs'
 import ajv from 'ajv'
 import path from 'path'
+import DebugLogger from 'debug'
 
 const configFileName = 'gigsboat.json'
 const __dirname = process.cwd()
 let gigsConfig = undefined
 
+const debug = DebugLogger('gigsboat:config')
+
 export async function getConfig(providedConfig) {
   if (gigsConfig) {
+    debug('using config from cache')
     return gigsConfig
   }
 
@@ -15,14 +19,16 @@ export async function getConfig(providedConfig) {
     gigsConfig = providedConfig
   } else {
     try {
+      debug('reading config from file: %s', configFileName)
       const jsonConfigFileContents = await fs.readFile(
         path.join(__dirname, configFileName),
         'utf8'
       )
       gigsConfig = JSON.parse(jsonConfigFileContents)
     } catch (error) {
-      // debug
-      // console.error(error)
+      // debug message and silently ignore
+      debug('encountered error while processing JSON config file:')
+      debug(error)
     }
   }
 
